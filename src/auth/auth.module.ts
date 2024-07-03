74521a7c6bd02eb79fa7cc3b1e8402a88c0461f4
CastanedaGT77 <castanedaGT77@gmail.com>
2024-07-03 00:33:59 -0600

    Changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,6 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserData } from 'src/user/models/data/UserData';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { RolesService } from 'src/roles/roles.service';
 import { RoleData } from 'src/roles/models/data/RoleData';
 import { PermissionData } from 'src/roles/models/data/PermissionData';
@@ -13,11 +11,6 @@ import { RolePermissionData } from 'src/roles/models/data/RolePermissionData';
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserData, RoleData, PermissionData, RolePermissionData]),
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: {expiresIn: '10800s'}
-    })
   ],
   controllers: [AuthController],
   providers: [AuthService, RolesService]
